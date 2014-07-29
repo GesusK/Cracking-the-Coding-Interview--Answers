c8974c2db09de5184521658461a19387a5e1eae8
lenovo <yx1232287@gmail.com>
2014-07-29 16:54:26 +0800

    question 1.1
    ver. 1.0

---
+++ TestJava.java
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 2014/7/29.
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ cc1_1/UniqueCharacters.java
@@ -0,0 +1,38 @@
+package cc1_1;
+
+/**
+ * Created by lenovo on 2014/7/28.
+ */
+public class UniqueCharacters {
+
+    public static void main(String[] args) {
+        String input1 = "qweJKG";
+        String input2 = "aebqcwB";
+
+        UniqueCharacters uc = new UniqueCharacters();
+        if(uc.isUnique(input1)) {
+            System.out.println(input1 + " has all unique characters.");
+        }else {
+            System.out.println(input1 + " has repeated characters.");
+        }
+        if(uc.isUnique(input2)) {
+            System.out.println(input2 + " has all unique characters.");
+        }else {
+            System.out.println(input2 + " has repeated characters.");
+        }
+    }
+
+    private boolean isUnique (String input) {
+        input = input.toUpperCase();
+        int[] check = new int[26];
+        for(int i = 0; i < input.length(); i ++) {
+            int checkIndex = input.charAt(i) - 'A';
+            if(check[checkIndex] == 1) {
+                return false;
+            }else {
+                check[checkIndex] = 1;
+            }
+        }
+        return true;
+    }
+}
