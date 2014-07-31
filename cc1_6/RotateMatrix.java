ad89e519e93803f7bd06f59178ede819b42a5704
Xiang Yan <yx1232287@gmail.com>
2014-07-31 15:47:35 +0800

    test

--- cc1_6/RotateMatrix.java
+++ cc1_6/RotateMatrix.java
@@ -17,7 +17,7 @@ public class RotateMatrix {
 
         for(int i = 0; i < m.length; i++) {
             for(int j = 0; j < m[i].length; j++) {
-                System.out.print(m[i][j]+" ");
+                System.out.print(m[i][j]+" "); 
             }
             System.out.println();
         }
