d3630c369310bb112df5a226975b0fb07e68f886
Xiang Yan <yx1232287@gmail.com>
2014-08-01 17:29:43 +0800

    question 2.3

---
+++ cc2_3/DeleteMiddle.java
@@ -0,0 +1,39 @@
+package cc2_3;
+
+import tools.MyNode;
+
+/**
+ * Created by lenovo on 2014/8/1.
+ */
+public class DeleteMiddle {
+    public static void main(String[] args) {
+        MyNode<Integer> e = new MyNode<Integer>(5, null);
+        MyNode<Integer> d = new MyNode<Integer>(4, e);
+        MyNode<Integer> c = new MyNode<Integer>(3, d);
+        MyNode<Integer> b = new MyNode<Integer>(2, c);
+        MyNode<Integer> a = new MyNode<Integer>(1, b);
+
+        DeleteMiddle dm = new DeleteMiddle();
+        dm.deleteMiddle(c);
+
+        MyNode<Integer> p = a;
+        while(p != null) {
+            System.out.print(p.getValue() + " ");
+            p = p.getNext();
+        }
+    }
+
+    private void deleteMiddle(MyNode m) {
+        if(m == null) {
+            return;
+        }
+        while(m.getNext() != null) {
+            m.setValue(m.getNext().getValue());
+            if(m.getNext().getNext() != null) {
+                m = m.getNext();
+            }else {
+                m.setNext(null);
+            }
+        }
+    }
+}
