bcafe021310b65ece7a60538886293393387b746
Xiang Yan <yx1232287@gmail.com>
2014-08-12 20:52:37 +0800

    question 3.6
    sort a stack

--- cc3_6/SortStack.java
+++ cc3_6/SortStack.java
@@ -18,12 +18,63 @@ public class SortStack {
         stack.push(3);
         stack.push(9);
         ss.sort(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 
     public void sort(Stack<Integer> stack0) {
+        // bidirectional sort
         int n = stack0.size();
         Stack<Integer> stack1 = new Stack<Integer>();
         int i = 0;
         int j = 0;
+        boolean isStack0 = true;
+        while(i+j != n) {
+            if(isStack0) {
+                int count = 0;
+                int tempMin = 999999;
+                while(count < n-i-j) {
+                    int num = stack0.pop();
+                    if(num < tempMin) {
+                        if(count != 0) {
+                            stack1.push(tempMin);
+                        }
+                        tempMin = num;
+                    }else {
+                        stack1.push(num);
+                    }
+                    count++;
+                }
+                stack0.push(tempMin);
+                isStack0 = false;
+                i++;
+            }else {
+                int count = 0;
+                int tempMax = -1;
+                while(count < n-j-i) {
+                    int num = stack1.pop();
+                    if(num > tempMax) {
+                        if(count != 0) {
+                            stack0.push(tempMax);
+                        }
+                        tempMax = num;
+                    }else {
+                        stack0.push(num);
+                    }
+                    count++;
+                }
+                stack1.push(tempMax);
+                isStack0 = true;
+                j++;
+            }
+        }
+        while(!stack1.empty()) {
+            stack0.push(stack1.pop());
+        }
     }
 }
