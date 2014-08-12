7957ec1c7aefc5d39adeccd3f1af0f77cf4ae27f
Xiang Yan <yx1232287@gmail.com>
2014-08-12 17:48:48 +0800

    question 3.5
    optimization

--- cc3_5/MyQueue.java
+++ cc3_5/MyQueue.java
@@ -8,30 +8,21 @@ import java.util.Stack;
 public class MyQueue<T> {
     private Stack<T> stackAdding;
     private Stack<T> stackRemoving;
-    private boolean lastAdding;
 
     public MyQueue() {
         this.stackAdding = new Stack<T>();
         this.stackRemoving = new Stack<T>();
-        this.lastAdding = true;
     }
 
     public void add(T t) {
-        if(!lastAdding) {
-            while(!stackRemoving.empty()) {
-                stackAdding.push(stackRemoving.pop());
-            }
-            lastAdding = true;
-        }
         stackAdding.push(t);
     }
 
     public T remove() {
-        if(lastAdding) {
+        if(stackRemoving.empty()) {
             while(!stackAdding.empty()) {
                 stackRemoving.push(stackAdding.pop());
             }
-            lastAdding = false;
         }
         return stackRemoving.pop();
     }
